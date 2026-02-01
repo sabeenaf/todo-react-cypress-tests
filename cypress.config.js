7f250e0d67a13d75f9a3ef36d8736128f41e2a05
sabeenaf <sabeenaf@github.com>
2026-02-01 10:22:32 +0100

    Adding config dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://example.cypress.io',      //  demo web app
+    supportFile: 'cypress/support/e2e.js',     //  support file
+
+    // Folder for screenshots and videos
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+
+    // Capture screenshots on failure
+    screenshotOnRunFailure: true,
+
+    // Record video of test runs
+    video: true,
+
+    // Optional: Node event listeners for plugins
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
